65abd165715802245386b60978054b0071afb721
Alex Lee <iasandcb@gmail.com>
2025-02-22 02:50:27 +0000

    Drop initial code

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 import app_model
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 model = app_model.AppModel()
 
@@ -12,3 +13,16 @@ model = app_model.AppModel()
 def say_app(text: str = Query()):
     response = model.get_response(text)
     return {"content" :response.content}
+
+@app.get("/translate")
+def translate(text: str = Query(), language: str = Query()):
+    response = model.get_prompt_response(language, text)
+    return {"content" :response.content}
+
+@app.get("/says")
+def say_app_stream(text: str = Query()):
+    def event_stream():
+        for message in model.get_streaming_response(text):
+            yield f"data: {message.content}\n\n"
+            
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
