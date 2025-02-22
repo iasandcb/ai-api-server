7613b62b41623db37d5eadbcbd4d2016cd7d28c4
Alex Lee <iasandcb@gmail.com>
2025-02-22 03:49:09 +0000

    Add comments

--- app_model.py
+++ app_model.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 
 class AppModel:
   def __init__(self):
+    # 환경변수를 로딩한다.
     load_dotenv() 
     self.model = init_chat_model("gpt-4o-mini", model_provider="openai")
     system_template = "Translate the following from English into {language}"
+    # 시스템 역할과 사용자 역학을 다 사용한다.
     self.prompt_template = ChatPromptTemplate.from_messages(
       [("system", system_template), ("user", "{text}")]
     )
--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from fastapi.staticfiles import StaticFiles
 import app_model
 
 app = FastAPI()
+
+# 정적 페이지를 제공하기 위한 설정을 한다. 
+# 이후 static 디렉토리에 파일을 두면 /static/ 밑으로 요청한다.
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 model = app_model.AppModel()
@@ -19,6 +22,7 @@ def translate(text: str = Query(), language: str = Query()):
     response = model.get_prompt_response(language, text)
     return {"content" :response.content}
 
+# SSE 기술을 써서 이벤트 스트림으로 내려준다. 클라이언트측 코드는 static/index.html을 참고하자.
 @app.get("/says")
 def say_app_stream(text: str = Query()):
     def event_stream():
