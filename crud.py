edef7fc905234ae159e24c444eabacf7b5592a33
Alex Lee <iasandcb@gmail.com>
2025-03-22 06:27:54 +0000

    Refactor DB connection

--- crud.py
+++ crud.py
@@ -2,18 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
+import redis
+import pymysql
 
 load_dotenv() 
 
-class Article(BaseModel):
-    title: str
-    body: str
-
 app = FastAPI()
 
-import pymysql
-
-timeout = 10
+timeout = int(os.getenv('MYSQL_TIMEOUT'))
 connection = pymysql.connect(
   charset="utf8mb4",
   connect_timeout=timeout,
@@ -22,23 +18,30 @@ connection = pymysql.connect(
   host=os.getenv('MYSQL_HOST'),
   password=os.getenv('MYSQL_PW'),
   read_timeout=timeout,
-  port=os.getenv('MYSQL_PORT'),
+  port=int(os.getenv('MYSQL_PORT')),
   user=os.getenv('MYSQL_USER'),
   write_timeout=timeout,
 )
-  
-"""Basic connection example.
-"""
-
-import redis
 
 r = redis.Redis(
     host=os.getenv('REDIS_HOST'),
-    port=13863,
+    port=int(os.getenv('REDIS_PORT')),
     decode_responses=True,
     username=os.getenv('REDIS_USER'),
     password=os.getenv('REDIS_PW'),
 )
+
+class Article(BaseModel):
+    title: str
+    body: str
+
+@app.post("/articles")
+def write_article(article:Article):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO articles (title, body) VALUES('{article.title}', '{article.body}')")
+    finally:
+        pass
 
 @app.get("/articles")
 def read_articles():
@@ -46,7 +49,6 @@ def read_articles():
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM articles")
         result = cursor.fetchall()
-        print(result)
         return result
     finally:
         pass
@@ -54,35 +56,39 @@ def read_articles():
 @app.get("/articles/{id}")
 def read_article(id):
     try:
-        # TODO: read from cache (hint: result = r.get('foo'))
         cached = r.hgetall(id)
-        print('c', cached)
         if cached == {}:
-            print('first')
             cursor = connection.cursor()
             cursor.execute(f"SELECT * FROM articles WHERE id={id}")
             result = cursor.fetchall()[0]
             r.hmset(id, result)
             return result
         else:
-            print('cached')
             return cached
     finally:
         pass
 
 @app.put("/articles/{id}")
 def update_article(id, article:Article):
-    # DB
     title = article.title
     body = article.body
-    print(title, body)
     try:
         cursor = connection.cursor()
-        query = f"UPDATE articles SET title='{title}', body='{body}' WHERE id={id}"
-        print(query)
+        query = f"UPDATE articles SET title='{article.title}', body='{article.body}' WHERE id={id}"
+        cursor.execute(query)
+    finally:
+        pass
+
+    r.hmset(id, {'id': id, 'title': title, 'body': body})
+
+@app.delete("/articles/{id}")
+def delete_article(id):
+    try:
+        cursor = connection.cursor()
+        query = f"DELETE FROM articles WHERE id={id}"
         cursor.execute(query)
     finally:
         pass
 
     # Cache
-    r.hmset(id, {'id': id, 'title': title, 'body': body})
+    r.delete(id)
